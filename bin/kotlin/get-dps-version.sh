b99cd320e8f87c2e27b45e887f4dd97cf690261a
Connor Glynn <connor.glynn@digital.justice.gov.uk>
2021-12-13 17:43:08 +0000

    update get dps version to handle java build files

--- bin/kotlin/get-dps-version.sh
+++ bin/kotlin/get-dps-version.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 set -e
-for file in $(cat ~/git/scripts/services/kotlin/dps-shared.txt);
-do 
+for file in $(cat ~/git/scripts/services/all/holding.txt);
+do
+    cd ~/git
+    if [ ! -d "$HOME/git/$file" ]
+    then
+        git clone "https://github.com/ministryofjustice/$file.git" &> /dev/null
+    fi
+
     echo processing: ${file}
-    cd ~/git/$file
-    cat ./build.gradle.kts | grep uk.gov.justice.hmpps.gradle-spring-boot
-    cd ~/
+    {
+      cd ~/git/$file
+      git stash
+      git checkout main
+      git pull
+    } &> /dev/null
+
+    # determine kotlin or java build file
+    if test -f "$HOME/git/$file/build.gradle.kts"
+    then
+      GRADLE_FILE=build.gradle.kts
+    else
+      GRADLE_FILE=build.gradle
+    fi
+
+    echo $file >> ~/Desktop/versions.txt
+    cat ./$GRADLE_FILE | grep uk.gov.justice.hmpps.gradle-spring-boot >> ~/Desktop/versions.txt
 done
