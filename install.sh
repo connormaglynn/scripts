c1220ce623b901f4635774b6ff7a41b5532f04c9
Connor Glynn <connor.glynn@digital.justice.gov.uk>
2021-12-20 15:03:31 +0000

    attempt an install script

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo PATH="~/git/scripts/bin/kotlin:${PATH}" | grep '\$' >> ~/.zshrc
+echo PATH="~/git/scripts/bin/git:${PATH}" | grep '\$' >> ~/.zshrc
+echo PATH="~/git/scripts/bin/kubernetes:${PATH}" | grep '\$' >> ~/.zshrc
+echo PATH="~/git/scripts/bin/node:${PATH}" | grep '\$' >> ~/.zshrc
+export PATH
