6eca2aaf634425640b9ed51400da8f0ef5525de9
Connor Glynn <connor.glynn@digital.justice.gov.uk>
2021-12-21 14:52:12 +0000

    update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
-echo PATH="~/git/scripts/bin/kotlin:${PATH}" | grep '\$' >> ~/.zshrc
-echo PATH="~/git/scripts/bin/git:${PATH}" | grep '\$' >> ~/.zshrc
-echo PATH="~/git/scripts/bin/kubernetes:${PATH}" | grep '\$' >> ~/.zshrc
-echo PATH="~/git/scripts/bin/node:${PATH}" | grep '\$' >> ~/.zshrc
-export PATH
+if [ ! -d "~/.zshrc" ]; then
+  SHELL_FILE=~/.zshrc
+else
+   SHELL_FILE=~/.bashrc
+fi
+
+echo "Installing scripts on PATH in $SHELL_FILE"
+echo '#--- scripts installation ---' >> $SHELL_FILE
+echo PATH='~/git/scripts/bin/kotlin:${PATH}' >> $SHELL_FILE
+echo PATH='~/git/scripts/bin/git:${PATH}' >> $SHELL_FILE
+echo PATH='~/git/scripts/bin/kubernetes:${PATH}' >> $SHELL_FILE
+echo PATH='~/git/scripts/bin/node:${PATH}' >>$SHELL_FILE
+echo PATH='~/git/scripts/bin/auth:${PATH}' >>$SHELL_FILE
+echo 'export PATH' >> $SHELL_FILE
+echo '#--- scripts installation ---' >> $SHELL_FILE
+
+source $SHELL_FILE
+echo "Path value: $PATH"
