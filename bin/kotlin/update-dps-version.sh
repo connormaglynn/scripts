b0feea076c457c437f1c2ea321fbe9ff0db16b7f
connormaglynn <connor.glynn@digital.justice.gov.uk>
2022-11-04 11:17:23 +0000

    ✨ Add option to update kotlin version with dps-plugin version

--- bin/kotlin/update-dps-version.sh
+++ bin/kotlin/update-dps-version.sh
@@ -6,6 +6,7 @@ VERSION=${2:?"missing arg 2 for VERSION"}
 SERVICES=${3:?"missing arg 3 for SERVICES"}
 COMMIT=${4:-false}
 PR=${5:-false}
+KOTLIN_VERSION=${6:-false}
 
 BRANCH_NAME="$TICKET-bump-gradle-spring-boot-version"
 MESSAGE="Bump gradle-spring-boot version to $VERSION"
@@ -17,7 +18,14 @@ do
   echo "updating $line to version $VERSION"
   sed -i "" "s/.*uk.gov.justice.hmpps.gradle-spring-boot.*/  id(\"uk.gov.justice.hmpps.gradle-spring-boot\") version \"$VERSION\"/" ./build.gradle.kts
 
-  ./gradlew clean build
+  if [ "$KOTLIN_VERSION" != false ]
+  then
+    echo "Kotlin version given, updating $line to version $KOTLIN_VERSION"
+    sed -i "" "s/.*plugin.jpa.*/  kotlin(\"plugin.jpa\") version \"$KOTLIN_VERSION\"/" ./build.gradle.kts
+    sed -i "" "s/.*plugin.spring.*/  kotlin(\"plugin.spring\") version \"$KOTLIN_VERSION\"/" ./build.gradle.kts
+  fi
+
+  ./gradlew clean build &> /dev/null < /dev/null
 
   source git-commit-to-branch.sh "$TICKET" "$MESSAGE" "$BRANCH_NAME" "$COMMIT"
   source git-create-pr.sh "$TICKET" "$MESSAGE" "$BRANCH_NAME" "$PR"
